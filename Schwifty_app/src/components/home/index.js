9dd952b1c89e6e4960418b950e91874832ca1820
Harbans Singh <harbans.tech@gmail.com>
2021-12-03 00:30:03 +0530

    Dashboard component created for Home component, along with Card component

--- Schwifty_app/src/components/home/index.js
+++ Schwifty_app/src/components/home/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styles from "./style"
 import Topbar from './TopBar/TopBar';
+import Dashboard from './Dashboard/Dashboard';
 
 const Home = () => {
     return (
         <View style={styles.homeTopbar}>
             <Topbar />
+            <Dashboard />
         </View>
     );
 }
