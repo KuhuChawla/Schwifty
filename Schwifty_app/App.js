d404feb50899560a372109580326fb895ba52542
gurnameh-99 <gurunameh.work@gmail.com>
2021-12-02 20:40:51 +0530

    JWT Auth async storage

--- Schwifty_app/App.js
+++ Schwifty_app/App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Loading from './src/components/common/loading';
+import Auth from './src/screens/auth';
+import LoggedIn from './src/screens/loggedIn';
+import deviceStorage from './src/services/deviceStorage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jwt: '',
+      loading: true
+    }
+
+    this.newJWT = this.newJWT.bind(this);
+    this.deleteJWT = deviceStorage.deleteJWT.bind(this);
+    this.loadJWT = deviceStorage.loadJWT.bind(this);
+    this.loadJWT();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <Loading size={'large'} />
+       );
+    } else if (!this.state.jwt) {
+      return (
+        <Auth newJWT={this.newJWT} />
+      );
+    } else if (this.state.jwt) {
+      return (
+        <LoggedIn jwt={this.state.jwt} deleteJWT={this.deleteJWT} />
+      );
+    }
+  }
+
+  newJWT(jwt){
+    this.setState({
+      jwt: jwt
+    });
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
