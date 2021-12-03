7c43ed8fc65ca6bbcd8babfd4edbb58c3d962cdd
Ashwani Rathee <ab669522@gmail.com>
2021-12-03 17:42:35 +0530

    initial_presentation

--- Schwifty_app/src/components/home/style.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-    homeTopbar: {
-      backgroundColor: '#2a2b4d',
-      width: '100%',
-      height: '100%',
-    },
-  });
--- backend/src/txn.py
+++
@@ -1,5 +0,0 @@
-from . import auth
-from . import models
-from flask import Blueprint, jsonify, request, current_app
-
-txn_blueprint = Blueprint("transaction", __name__)
